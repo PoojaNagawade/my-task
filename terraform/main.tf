44024b8ce18c5f130a4ba2ca82977765e7fcf765
PoojaNagawade <156826856+PoojaNagawade@users.noreply.github.com>
2024-03-24 21:46:01 +0100

    Update main.tf
    added policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,9 +117,44 @@ resource "aws_security_group" "jenkins_sg" {
   }
 }
 
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicRead",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject"],
+      "Resource": [
+        "arn:aws:s3:::aws_s3_bucket.public_bucket.arn/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+
+
 resource "aws_s3_bucket" "private_bucket" {
   bucket = var.private_bucket_name
-  
+  versioning {
+      enabled = true
+    }
   tags = {
     Name = "My Private Bucket"
   }
@@ -127,6 +162,9 @@ resource "aws_s3_bucket" "private_bucket" {
 
 resource "aws_s3_bucket" "public_bucket" {
   bucket = var.public_bucket_name
+  versioning {
+      enabled = true
+    }
   
   tags = {
     Name = "My Public Bucket"
